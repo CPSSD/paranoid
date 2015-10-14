cb6fce25504b7402c7709c7c0800a9ceb71b5cb0
Terry Bolt <bolt.terry@gmail.com>
2015-10-14 21:26:37 +0100

    Finished basic echo server. Need to write unit tests and maybe tweak code.

--- network/server/main.go
+++ network/server/main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
 )
+
+var websocketUpgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 type connPool struct {
 	connections map[*connection]bool
@@ -56,5 +63,55 @@ func NewConnection(ws *websocket.Conn, p *connPool) *connection {
 		ws:       ws,
 		p:        p,
 		messages: make(chan []byte, 256),
+	}
+}
+
+func (conn *connection) Reader() {
+	for {
+		_, message, err := conn.ws.ReadMessage()
+		if err != nil {
+			log.Println("ERROR: Could not read message: ", err)
+			break
+		}
+		conn.p.messages <- message
+	}
+	conn.ws.Close()
+}
+
+func (conn *connection) Writer() {
+	for message := range conn.messages {
+		err := conn.ws.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println("ERROR: Could not write to websocket: ", err)
+			break
+		}
+	}
+	conn.ws.Close()
+}
+
+type websocketHandler struct {
+	p *connPool
+}
+
+func (handler websocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("ERROR: Could not upgrade websocket: ", err)
+		return
+	}
+	conn := NewConnection(ws, handler.p)
+	handler.p.Register <- conn
+	defer func() { handler.p.Unregister <- conn }()
+	go conn.Writer()
+	conn.Reader()
+}
+
+func main() {
+	pool := NewConnPool()
+	go pool.Run()
+	http.Handle("/connect", websocketHandler{p: pool})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln("FATAL: ", err)
 	}
 }
