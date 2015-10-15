24e5522ced242a666424de58ad1ba219a22b6386
Sean Healy <sean.healy33@mail.dcu.ie>
2015-10-15 18:42:51 +0100

    Basic Dummy client

---
+++ pfs_dummy/pfs_dummy.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+read_fn () {
+  OPTION=$1
+  OFFSET=$2
+
+  if [ "$OFFSET" = "" ]
+    then
+      echo "Dummy Content from Fake file"
+  else
+    echo "Dummy Content"
+  fi
+}
+
+write_fn (){
+  TYPE=$1
+
+  if [ "$TYPE" = "-f" ]
+    then
+      exit 0
+  elif [ "$TYPE" = "-n" ]
+    then
+      exit 0
+  else
+    echo "No option provided?"
+  fi
+}
+
+readdir_fn (){
+  echo "my_file.sh"
+}
+
+if [ "$1" = "read" ]
+  then
+    read_fn $2 $3
+elif [ "$1" = "readdir" ]
+  then
+    readdir_fn
+elif [ "$1" = "write" ]
+  then
+    write_fn $2
+else
+  echo "Missing one or more args"
+fi
