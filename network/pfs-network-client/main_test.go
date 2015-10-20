f6a430c0d3f9b2d2923f096229d31619c6affa85
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2015-10-20 04:18:26 +0100

    Added the pfs-network-client testing, currently fails 2 tests for unknown reasons. Will fix soon

---
+++ network/pfs-network-client/main_test.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"testing"
+)
+
+// Stores the messageData for each parsed message type
+var (
+	md1 messageData
+	md2 messageData
+	md3 messageData
+	md4 messageData
+	md5 messageData
+)
+
+func TestParseMessage(t *testing.T) {
+	// Write
+	fmd1 := []byte(`
+		{
+			"sender": "abc",
+			"type": "write",
+			"name": "_test.txt",
+			"offset": 0,
+			"length": 8,
+			"data":"aGVsbG8="
+		}
+	`)
+
+	// Creat
+	fmd2 := []byte(`
+		{
+			"sender": "abc",
+			"type": "creat",
+			"name": "_test.txt"
+		}
+	`)
+
+	// Link
+	fmd3 := []byte(`
+		{
+			"sender": "abc",
+			"type": "link",
+			"name": "_test.txt",
+			"target": "_test2.txt"
+		}
+	`)
+
+	// Unlink
+	fmd4 := []byte(`
+		{
+			"sender": "abc",
+			"type": "write",
+			"name": "_test.txt"
+		}
+	`)
+
+	// truncate
+	fmd5 := []byte(`
+		{
+			"sender": "abc",
+			"type": "write",
+			"name": "_test.txt",
+			"offset": 0
+		}
+	`)
+
+	// Check is each message parsed without errors
+	md1p, err := parseMessage(fmd1)
+	if err != nil {
+		t.Error("parseMessage failed on write")
+	}
+	md1 = md1p
+
+	md2p, err := parseMessage(fmd2)
+	if err != nil {
+		t.Error("parseMessage failed on creat")
+	}
+	md2 = md2p
+
+	md3p, err := parseMessage(fmd3)
+	if err != nil {
+		t.Error("parseMessage failed on link")
+	}
+	md3 = md3p
+
+	md4p, err := parseMessage(fmd4)
+	if err != nil {
+		t.Error("parseMessage failed on unlink")
+	}
+	md4 = md4p
+
+	md5p, err := parseMessage(fmd5)
+	if err != nil {
+		t.Error("parseMessage failed on truncate")
+	}
+	md5 = md5p
+}
+
+func TestHasValidFields(t *testing.T) {
+	if err := hasValidFields(md1); err != nil {
+		t.Error("hasValidFields failed on write")
+	}
+
+	if err := hasValidFields(md2); err != nil {
+		t.Error("hasValidFields failed on creat")
+	}
+
+	if err := hasValidFields(md3); err != nil {
+		t.Error("hasValidFields failed on link")
+	}
+
+	if err := hasValidFields(md4); err != nil {
+		t.Error("hasValidFields failed on unlink")
+	}
+
+	if err := hasValidFields(md5); err != nil {
+		t.Error("hasValidFields failed on truncate")
+	}
+}
