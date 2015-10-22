752ea1605617ab53fa0705a34011daad49fa4d7f
Sean Healy <sean.healy33@mail.dcu.ie>
2015-10-22 13:41:18 +0100

    Added stat pfs to PFS dummy

--- pfs_dummy/pfs_dummy.sh
+++ pfs_dummy/pfs_dummy.sh
@@ -32,12 +32,19 @@ readdir_fn (){
   echo "my_file.sh"
 }
 
+stat_fn(){
+  echo "{\"length\":10,\"ctime\":\"2015-10-22T03:08:47.541016+01:00\",\"mtime\":\"2015-10-22T03:08:47.541016+01:00\",\"atime\":\"2015-10-22T03:07:33.877016+01:00\"}"
+}
+
 if [ "$1" = "read" ]
   then
     read_fn $2 $3
 elif [ "$1" = "readdir" ]
   then
     readdir_fn
+elif [ "$1" = "stat" ]
+  then
+    stat_fn
 elif [ "$1" = "write" ]
   then
     write_fn $2
