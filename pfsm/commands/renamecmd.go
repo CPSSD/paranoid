f2fec1738cbea80a607a3ccdde50d35c6361c978
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-06 13:14:09 +0000

    added rename case in main.go of pfsm

--- pfsm/commands/renamecmd.go
+++ pfsm/commands/renamecmd.go
@@ -8,3 +8,7 @@ import (
 	"os"
 	"path"
 )
+
+func RenameCommand(args []string) {
+
+}
--- pfsm/main.go
+++ pfsm/main.go
@@ -59,6 +59,8 @@ func main() {
 			commands.TruncateCommand(onlyArgs[1:])
 		case "utimes":
 			commands.UtimesCommand(onlyArgs[1:])
+		case "rename":
+			commands.RenameCommand(onlyArgs[1:])
 		default:
 			log.Fatalln("Given command not recognised")
 		}
