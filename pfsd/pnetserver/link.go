a78222aef1697f6e4faf9c14460dc0abeda7b199
Terry Bolt <bolt.terry@gmail.com>
2015-11-08 14:00:09 +0000

    Added link and unlink handlers.

--- pfsd/pnetserver/link.go
+++ pfsd/pnetserver/link.go
@@ -1,12 +1,56 @@
 package pnetserver
 
 import (
+	"bufio"
+	"github.com/cpssd/paranoid/pfsm/returncodes"
 	pb "github.com/cpssd/paranoid/proto/paranoidnetwork"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"log"
+	"os/exec"
+	"strconv"
 )
 
 func (s *ParanoidServer) Link(ctx context.Context, req *pb.LinkRequest) (*pb.EmptyMessage, error) {
-	log.Printf("ERROR: Link not yet implemented.")
-	return nil, nil
+	command := exec.Command("pfsm", "-n", "link", ParanoidDir, req.OldPath, req.NewPath)
+	stdout, err := command.StdoutPipe()
+	if err != nil {
+		log.Println("ERROR: Could not capture stdout of subprocess.")
+	}
+	read := bufio.NewReader(stdout)
+	err = command.Run()
+	if err != nil {
+		log.Printf("ERROR: Could not link file %s to %s: %v.\n", req.OldPath, req.NewPath, err)
+		returnError := grpc.Errorf(codes.Internal, "could not link file %s to %s: %v",
+			req.OldPath, req.NewPath, err)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	pfsmoutput, _, err := read.ReadLine()
+	codeBytes := pfsmoutput[:2]
+	code, err := strconv.Atoi(string(codeBytes))
+	if err != nil {
+		log.Println("ERROR: Could not interpret PFSM error code.")
+		returnError := grpc.Errorf(codes.Internal, "could not link file %s to %s: %v",
+			req.OldPath, req.NewPath, err)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	switch code {
+	case returncodes.EACCES:
+		log.Printf("INFO: Do not have permission to edit %s.\n", req.OldPath)
+		returnError := grpc.Errorf(codes.PermissionDenied,
+			"do not have permission to edit %s",
+			req.OldPath)
+		return &pb.EmptyMessage{}, returnError
+	case returncodes.ENOENT:
+		log.Printf("INFO: File %s does not exist.\n", req.OldPath)
+		returnError := grpc.Errorf(codes.NotFound,
+			"file %s does not exist",
+			req.OldPath)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	return &pb.EmptyMessage{}, nil
 }
--- pfsd/pnetserver/unlink.go
+++ pfsd/pnetserver/unlink.go
@@ -1,12 +1,54 @@
 package pnetserver
 
 import (
+	"bufio"
+	"github.com/cpssd/paranoid/pfsm/returncodes"
 	pb "github.com/cpssd/paranoid/proto/paranoidnetwork"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"log"
+	"os/exec"
+	"strconv"
 )
 
 func (s *ParanoidServer) Unlink(ctx context.Context, req *pb.UnlinkRequest) (*pb.EmptyMessage, error) {
-	log.Printf("ERROR: Unlink not yet implemented.")
-	return nil, nil
+	command := exec.Command("pfsm", "-n", "unlink", ParanoidDir, req.Path)
+	stdout, err := command.StdoutPipe()
+	if err != nil {
+		log.Println("ERROR: Could not capture stdout of subprocess.")
+	}
+	read := bufio.NewReader(stdout)
+	err = command.Run()
+	if err != nil {
+		log.Printf("ERROR: Could not unlink file %s: %v.\n", req.Path, err)
+		returnError := grpc.Errorf(codes.Internal, "could not unlink file %s: %v", req.Path, err)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	pfsmoutput, _, err := read.ReadLine()
+	codeBytes := pfsmoutput[:2]
+	code, err := strconv.Atoi(string(codeBytes))
+	if err != nil {
+		log.Println("ERROR: Could not interpret PFSM error code.")
+		returnError := grpc.Errorf(codes.Internal, "could not unlink file %s: %v", req.Path, err)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	switch code {
+	case returncodes.EACCES:
+		log.Printf("INFO: Do not have permission to edit %s.\n", req.Path)
+		returnError := grpc.Errorf(codes.PermissionDenied,
+			"do not have permission to edit %s",
+			req.Path)
+		return &pb.EmptyMessage{}, returnError
+	case returncodes.ENOENT:
+		log.Printf("INFO: File %s does not exist.\n", req.Path)
+		returnError := grpc.Errorf(codes.NotFound,
+			"file %s does not exist",
+			req.Path)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	return &pb.EmptyMessage{}, nil
 }
