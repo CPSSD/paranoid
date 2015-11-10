7415b2609cad4b8ce0e6349873feed8d9492240c
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-10 18:27:18 +0000

    added verbose logging capability to icserver

--- ic/icserver/server.go
+++ ic/icserver/server.go
@@ -6,21 +6,46 @@ import (
 	"os"
 )
 
+var verbose = false
+
+// handleConnection accepts a connection and handles messages received through the connection
+func handleConnection(conn net.Conn) {
+	verboseLog("icserver new connection")
+	defer verboseLog("icserver connection lost")
+	for {
+
+	}
+}
+
 // RunServer runs the server
-func RunServer() {
+// give a true parameter for verbose logging
+func RunServer(verboseLogging bool) {
+	verbose = verboseLogging
+
 	sockFIlePath := "/tmp/pfic.sock"
 	listener, err := net.Listen("unix", sockFIlePath)
 	if err != nil {
 		log.Fatalln("ic listen error: ", err)
 	}
-	defer os.Remove(sockFIlePath)
 
+	defer os.Remove(sockFIlePath)
+	defer verboseLog("icserver no longer listening")
+
+	verboseLog("icserver listening on " + sockFIlePath)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatalln("ic accept error: ", err)
 		}
 
-		// send somewhere
+		go handleConnection(conn)
+	}
+}
+
+// verboseLog logs what the server is doing if the verboseLogging option was
+// given when running the server
+func verboseLog(message string) {
+	if verbose {
+		log.Println(message)
 	}
 }
