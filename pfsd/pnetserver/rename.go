25a51d5b6d4c6a2124c9e9b2e82c0886fd0102a0
Terry Bolt <bolt.terry@gmail.com>
2015-11-10 17:04:23 +0000

    Added handlers for chmod and rename.

--- pfsd/pnetserver/rename.go
+++ pfsd/pnetserver/rename.go
@@ -3,10 +3,21 @@ package pnetserver
 import (
 	pb "github.com/cpssd/paranoid/proto/paranoidnetwork"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"log"
 )
 
 func (s *ParanoidServer) Rename(ctx context.Context, req *pb.RenameRequest) (*pb.EmptyMessage, error) {
-	log.Printf("ERROR: Rename not yet implemented.")
-	return nil, nil
+	code, _, err := runCommand(nil, "rename", ParanoidDir, req.OldPath, req.NewPath)
+	if err != nil {
+		log.Printf("ERROR: Could not rename file %s: %v.\n", req.OldPath, err)
+		returnError := grpc.Errorf(codes.Internal, "could not rename file %s: %v",
+			req.OldPath, err)
+		return &pb.EmptyMessage{}, returnError
+	}
+
+	returnError := convertCodeToError(code, req.OldPath)
+	// If returnError is nil here, it's equivalent to returning OK
+	return &pb.EmptyMessage{}, returnError
 }
