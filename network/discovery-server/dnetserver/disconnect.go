fecae8ac75991f1a7970378a151ede0bc2ea07d4
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2015-11-12 19:40:21 +0000

    Simplified code

--- network/discovery-server/dnetserver/disconnect.go
+++ network/discovery-server/dnetserver/disconnect.go
@@ -11,20 +11,15 @@ import (
 
 // Disconnect method for Discovery Server
 func (s *DiscoveryServer) Disconnect(ctx context.Context, req *pb.DisconnectRequest) (*pb.EmptyMessage, error) {
-	isInNodes := false
-
 	for i, node := range Nodes {
 		if reflect.DeepEqual(&node, req.Node) {
 			Nodes[i].Active = false
-			isInNodes = true
-			break
+			log.Printf("[I] Disconnect: Node %s:%s disconnected", req.Node.Ip, req.Node.Port)
+			return &pb.EmptyMessage{}, nil
 		}
 	}
 
-	var returnError error
-	if !isInNodes {
-		log.Printf("[E] Disconnect: Node %s:%s was not found\n", req.Node.Ip, req.Node.Port)
-		returnError = grpc.Errorf(codes.NotFound, "node was not found")
-	}
-	return &pb.EmptyMessage{}, returnError // returns nil if no error
+	log.Printf("[E] Disconnect: Node %s:%s was not found\n", req.Node.Ip, req.Node.Port)
+	returnError := grpc.Errorf(codes.NotFound, "node was not found")
+	return &pb.EmptyMessage{}, returnError
 }
