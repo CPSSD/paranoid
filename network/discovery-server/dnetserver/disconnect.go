86e097f72d7e45d57f0fe304a03389fe33c207fe
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2015-11-12 01:38:41 +0000

    Added Disconnect

--- network/discovery-server/dnetserver/disconnect.go
+++ network/discovery-server/dnetserver/disconnect.go
@@ -3,11 +3,25 @@ package dnetserver
 import (
 	pb "github.com/cpssd/paranoid/proto/discoverynetwork"
 	"golang.org/x/net/context"
-	"log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"reflect"
 )
 
 // Disconnect method for Discovery Server
 func (s *DiscoveryServer) Disconnect(ctx context.Context, req *pb.DisconnectRequest) (*pb.EmptyMessage, error) {
-	log.Printf("[E] Disconnect not yet implemented.\n")
-	return nil, nil
+	isInNodes := false
+
+	for i, node := range Nodes {
+		if reflect.DeepEqual(&node, req.Node) {
+			Nodes[i].Active = false
+			isInNodes = true
+		}
+	}
+
+	var returnError error
+	if !isInNodes {
+		returnError = grpc.Errorf(codes.NotFound, "node was not found")
+	}
+	return &pb.EmptyMessage{}, returnError // returns nil if no error
 }
