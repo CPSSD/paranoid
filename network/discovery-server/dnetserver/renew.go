bd53a297b73f607d8a741ee55b7fd97e1f77bf9b
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2015-11-12 01:54:58 +0000

    Added renew to discovery server

--- network/discovery-server/dnetserver/renew.go
+++ network/discovery-server/dnetserver/renew.go
@@ -3,11 +3,34 @@ package dnetserver
 import (
 	pb "github.com/cpssd/paranoid/proto/discoverynetwork"
 	"golang.org/x/net/context"
-	"log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"reflect"
+	"time"
 )
 
 // Renew method for Discovery Server
 func (s *DiscoveryServer) Renew(ctx context.Context, req *pb.JoinRequest) (*pb.EmptyMessage, error) {
-	log.Printf("[E] Renew not yet implemented.\n")
-	return nil, nil
+	isInNodes := false
+	isActiveNode := false
+
+	for i, node := range Nodes {
+		if reflect.DeepEqual(&node, req.Node) {
+			if node.Active {
+				Nodes[i].ExpiryTime = time.Now().Add(RenewInterval)
+				isActiveNode = true
+			}
+			isInNodes = true
+			break
+		}
+	}
+
+	var returnError error
+	if !isInNodes {
+		returnError = grpc.Errorf(codes.NotFound, "node was not found")
+	}
+	if !isActiveNode {
+		returnError = grpc.Errorf(codes.DeadlineExceeded, "renewal time is past deadline")
+	}
+	return &pb.EmptyMessage{}, returnError // returns nil if no error
 }
