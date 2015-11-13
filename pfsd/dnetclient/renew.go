a46277aeaf294f0f37eeb51ece63a831419efcc3
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2015-11-13 10:15:05 +0000

    Added a goroutine for renewing

--- pfsd/dnetclient/renew.go
+++ pfsd/dnetclient/renew.go
@@ -5,8 +5,11 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
+	"time"
 )
 
+// Renew function. Will create a goroutine which will send renew to server
+// 1/10 before expriration
 func Renew() {
 	conn, err := grpc.Dial(DiscoveryAddr, grpc.WithInsecure())
 	if err != nil {
@@ -16,10 +19,19 @@ func Renew() {
 	defer conn.Close()
 
 	dclient := pb.NewDiscoveryNetworkClient(conn)
+	pbNode := pb.Node{ThisNode.IP, ThisNode.Port}
 
-	_, err = dclient.Renew(context.Background(), &pb.JoinRequest{Pool: "_", Node: &thisNode})
-	if err != nil {
-		log.Println("[D] [E] could not join")
-		return
+	go callRenew(dclient, pbNode)
+}
+
+func callRenew(dclient pb.DiscoveryNetworkClient, pbNode pb.Node) {
+	for {
+		_, err := dclient.Renew(context.Background(), &pb.JoinRequest{Pool: "_", Node: &pbNode})
+		if err != nil {
+			log.Println("[D] [E] could not join")
+		}
+
+		time.Sleep(resetInterval)
 	}
+
 }
