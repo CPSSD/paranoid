ae94af7a493997cded56bdb79e9ac4e305ca2c51
Terry Bolt <bolt.terry@gmail.com>
2015-12-02 01:52:34 +0000

    Added function to generate TLS certificate.

---
+++ paranoid-cli/tls/gencert.go
@@ -0,0 +1,115 @@
+package tls
+
+import (
+	"bufio"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+	"os"
+	"path"
+	"strings"
+	"time"
+)
+
+// GenCertificate will generate a TLS cert and key, prompting the user
+// to enter relevant information. The resulting data will be saved to PEM
+// files inside outputDir called "cert.pem" and "key.pem".
+func GenCertificate(pfsDir string) error {
+	scanner := bufio.NewScanner(os.Stdin)
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return fmt.Errorf("could not generate private key: %v", err)
+	}
+
+	startDate := time.Now()
+	fmt.Print("Enter the length of time for which the cert will be valid, in days: ")
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("could not read user input: %v", err)
+	}
+	validForString := scanner.Text()
+	log.Println(validForString)
+	validFor, err := time.ParseDuration(validForString + "d")
+	endDate := startDate.Add(validFor)
+
+	// Sets max to 1 << 128
+	serialNumberMax := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberMax)
+	if err != nil {
+		return fmt.Errorf("could not generate serial number: %v", err)
+	}
+
+	fmt.Print("Enter the name of your organisation: ")
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("could not read user input: %v", err)
+	}
+	organisation := scanner.Text()
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			// American spelling :(
+			Organization: []string{organisation},
+		},
+		NotBefore:             startDate,
+		NotAfter:              endDate,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA: true,
+	}
+
+	fmt.Print("Enter a comma-separated list of hostnames and/or IP addresses this cert will validate: ")
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("could not read user input: %v", err)
+	}
+	hostsString := scanner.Text()
+
+	hosts := strings.Split(hostsString, ",")
+	for _, host := range hosts {
+		if ip := net.ParseIP(host); ip != nil {
+			template.IPAddresses = append(template.IPAddresses, ip)
+		} else {
+			template.DNSNames = append(template.DNSNames, host)
+		}
+	}
+
+	certBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return fmt.Errorf("failed to create certificate: %v", err)
+	}
+
+	certPath := path.Join(pfsDir, "meta", "cert.pem")
+	certFile, err := os.OpenFile(certPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	defer certFile.Close()
+	if err != nil {
+		return fmt.Errorf("failed to create cert file: %v", err)
+	}
+	pem.Encode(certFile, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+	log.Println("INFO: Wrote certificate to", certPath)
+
+	keyPath := path.Join(pfsDir, "meta", "key.pem")
+	keyFile, err := os.OpenFile(keyPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	defer keyFile.Close()
+	if err != nil {
+		return fmt.Errorf("failed to create key file: %v", err)
+	}
+	pem.Encode(keyFile, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	})
+	log.Println("INFO: Wrote key to", keyPath)
+
+	return nil
+}
---
+++ paranoid-cli/tls/gencert_test.go
@@ -0,0 +1,76 @@
+// +build !integration
+
+package tls
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+	"time"
+)
+
+func TestGenerateCert(t *testing.T) {
+	testPath := path.Join(os.TempDir(), "testCertGen")
+	os.RemoveAll(testPath)
+	os.Mkdir(testPath, 0777)
+	os.Mkdir(path.Join(testPath, "meta"), 0777)
+	// Since GenCertificate takes input from os.Stdin, we need to create
+	// a fake stdin.
+	fakeStdin, err := os.OpenFile(path.Join(testPath, "fakeStdin"),
+		os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+	defer fakeStdin.Close()
+	if err != nil {
+		t.Fatal("Failed to create fake stdin file:", err)
+	}
+	_, err = fakeStdin.WriteString("1\nParanoid Inc.\ntest.paranoid.com\n")
+	if err != nil {
+		t.Fatal("Failed to write to fake stdin file:", err)
+	}
+	// Force a flush of the newly-written data.
+	fakeStdin.Close()
+	fakeStdin, err = os.Open(path.Join(testPath, "fakeStdin"))
+	if err != nil {
+		t.Fatal("Failed to reopen fake stdin file:", err)
+	}
+	os.Stdin = fakeStdin
+	if err != nil {
+		t.Fatal("Failed to redirect stdout:", err)
+	}
+
+	err = GenCertificate(testPath)
+	if err != nil {
+		t.Fatal("GenCertificate returned error:", err)
+	}
+
+	// Now we read the generated cert to confirm everything went okay.
+	certPath := path.Join(testPath, "meta", "cert.pem")
+	encodedPem, err := ioutil.ReadFile(certPath)
+	if err != nil {
+		t.Fatalf("could not open %s: %v\n", certPath, err)
+	}
+	decodedPem, _ := pem.Decode(encodedPem)
+	if decodedPem == nil {
+		t.Fatal("could not decode PEM file")
+	}
+	cert, err := x509.ParseCertificate(decodedPem.Bytes)
+	if err != nil {
+		t.Fatal("could not parse certificate:", err)
+	}
+	if cert.Subject.Organization[0] != "Paranoid Inc." {
+		t.Error("Organisation field incorrect. Expected: \"Paranoid Inc.\". Actual:",
+			cert.Subject.Organization[0])
+	}
+	certDuration := cert.NotAfter.Sub(cert.NotBefore)
+	expectedDuration, _ := time.ParseDuration("365d")
+	if certDuration.Hours() != expectedDuration.Hours() {
+		t.Errorf("Certificate duration incorrect. Expected: 365 days. Actual: %.0f\n",
+			certDuration.Hours()/24)
+	}
+	if cert.DNSNames[0] != "test.paranoid.com" {
+		t.Error("DNS name incorrect. Expected: \"test.paranoid.com\". Actual:",
+			cert.DNSNames[0])
+	}
+}
