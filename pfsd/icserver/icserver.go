c640c65b9b63aa069aae4ee7b5fbcb2e80ac9cce
Conor Griffin <conor.griffin37@mail.dcu.ie>
2015-12-06 17:00:28 +0000

    Update icserver.go

--- pfsd/icserver/icserver.go
+++ pfsd/icserver/icserver.go
@@ -80,7 +80,6 @@ func StopAccept() {
 // RunServer runs the server
 // give a true parameter for verbose logging
 func RunServer(pfsDirectory string, verboseLogging bool) {
-	log.Println("Running IC server")
 	defer globals.Wait.Done()
 	sockFilePath := path.Join(pfsDirectory, "meta", "pfic.sock")
 	deleteSockFile(sockFilePath)
