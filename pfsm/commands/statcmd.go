b1eda6799aabd1eb1ef6379c3e2021207f1b32fe
Conor Griffin <conor.griffin37@mail.dcu.ie>
2015-12-15 17:43:42 +0000

    stat refactor

--- pfsm/commands/statcmd.go
+++ pfsm/commands/statcmd.go
@@ -1,9 +1,9 @@
 package commands
 
 import (
-	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/cpssd/paranoid/pfsm/returncodes"
-	"io"
 	"os"
 	"path"
 	"syscall"
@@ -11,49 +11,60 @@ import (
 )
 
 type statInfo struct {
-	Length int64       `json:"length",omitempty`
-	Ctime  time.Time   `json:"ctime",omitempty`
-	Mtime  time.Time   `json:"mtime",omitempty`
-	Atime  time.Time   `json:"atime",omitempty`
-	Mode   os.FileMode `json:"mode",omitempty`
+	Length int64
+	Ctime  time.Time
+	Mtime  time.Time
+	Atime  time.Time
+	Mode   os.FileMode
 }
 
 //StatCommand prints a json object containing information on the file given as args[1] in pfs directory args[0] to Stdout
 //Includes the length of the file, ctime, mtime and atime
-func StatCommand(args []string) {
+func StatCommand(directory, fileName string) (returnCode int, returnError error, info statInfo) {
 	Log.Info("Stat command called")
-	if len(args) < 2 {
-		Log.Fatal("Not enough arguments")
-	}
-	directory := args[0]
 	Log.Verbose("stat : given directory", directory)
 
-	getFileSystemLock(directory, sharedLock)
-	defer unLockFileSystem(directory)
+	err := getFileSystemLock(directory, sharedLock)
+	if err != nil {
+		return returncodes.EUNEXPECTED, err, statInfo{}
+	}
 
-	namepath := getParanoidPath(directory, args[1])
-	namePathType := getFileType(directory, namepath)
+	defer func() {
+		err := unLockFileSystem(directory)
+		if err != nil {
+			returnCode = returncodes.EUNEXPECTED
+			returnError = err
+			info = statInfo{}
+		}
+	}()
+
+	namepath := getParanoidPath(directory, fileName)
+	namePathType, err := getFileType(directory, namepath)
+	if err != nil {
+		return returncodes.EUNEXPECTED, fmt.Errorf("error getting "+fileName+" file type:", err), statInfo{}
+	}
+
 	if namePathType == typeENOENT {
-		io.WriteString(os.Stdout, returncodes.GetReturnCode(returncodes.ENOENT))
-		return
+		return returncodes.ENOENT, errors.New(fileName + " does not exist"), statInfo{}
 	}
 
-	fileNameBytes, code := getFileInode(namepath)
+	inodeBytes, code, err := getFileInode(namepath)
 	if code != returncodes.OK {
-		io.WriteString(os.Stdout, returncodes.GetReturnCode(code))
-		return
+		return code, err, statInfo{}
 	}
-	fileName := string(fileNameBytes)
-	contentsFile := path.Join(directory, "contents", fileName)
+
+	inodeName := string(inodeBytes)
+	contentsFile := path.Join(directory, "contents", inodeName)
 
 	fi, err := os.Lstat(contentsFile)
 	if err != nil {
-		Log.Fatal("error Lstating file:", err)
+		return returncodes.EUNEXPECTED, fmt.Errorf("error Lstating file:", err), statInfo{}
 	}
 
 	stat := fi.Sys().(*syscall.Stat_t)
 	atime := time.Unix(int64(stat.Atim.Sec), int64(stat.Atim.Nsec))
 	ctime := time.Unix(int64(stat.Ctim.Sec), int64(stat.Ctim.Nsec))
+
 	var mode os.FileMode
 	switch namePathType {
 	case typeDir:
@@ -69,11 +80,6 @@ func StatCommand(args []string) {
 		Atime:  atime,
 		Mode:   mode}
 
-	jsonData, err := json.Marshal(statData)
-	if err != nil {
-		Log.Fatal("error marshalling statData:", err)
-	}
-	Log.Verbose("stat : returning", string(jsonData))
-	io.WriteString(os.Stdout, returncodes.GetReturnCode(returncodes.OK))
-	io.WriteString(os.Stdout, string(jsonData))
+	Log.Verbose("stat : returning", statData)
+	return returncodes.OK, nil, *statData
 }
