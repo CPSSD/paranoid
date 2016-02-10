da6c1b326fbfb39a2bb3943cbba8a3d0fcbfcdbd
Terry Bolt <bolt.terry@gmail.com>
2016-02-10 15:59:19 +0000

    Wrote Key and KeyPiece classes. Wrote key deconstruction and reconstruction functions.

---
+++ pfsd/keyman/distribution_test.go
@@ -0,0 +1,31 @@
+package keyman
+
+import (
+	"testing"
+)
+
+func TestDistributionGoodRebuild(t *testing.T) {
+	keyBytes := []byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
+	key, _ := NewKey(keyBytes)
+	pieces, _ := GeneratePieces(key, 5, 3)
+
+	// Since we need at least 3 pieces to reconstruct the key, this should work.
+	_, err := RebuildKey(pieces[:3])
+	if err != nil {
+		t.Log(err.(*FingerMismatchError).ExpectedFingerprint)
+		t.Log(err.(*FingerMismatchError).ActualFingerprint)
+		t.Fatal(err)
+	}
+}
+
+func TestDistributionBadRebuild(t *testing.T) {
+	keyBytes := []byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
+	key, _ := NewKey(keyBytes)
+	pieces, _ := GeneratePieces(key, 5, 3)
+
+	// Since we need at least 3 pieces to reconstruct the key, this should not work.
+	_, err := RebuildKey(pieces[:2])
+	if err == nil {
+		t.Fatal("expected fingerprint mismatch")
+	}
+}
