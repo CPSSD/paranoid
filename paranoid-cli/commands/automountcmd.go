d7b1a0a7881c474486853ac3e40118cea6ccefde
Conor Griffin <conor.griffin37@mail.dcu.ie>
2016-03-30 13:51:35 +0100

    Fix automount command

--- paranoid-cli/commands/automountcmd.go
+++ paranoid-cli/commands/automountcmd.go
@@ -24,24 +24,12 @@ func AutoMount(c *cli.Context) {
 	}
 	pfsMeta := path.Join(usr.HomeDir, ".pfs", "filesystems", args[0], "meta")
 
-	ip, err := ioutil.ReadFile(path.Join(pfsMeta, "ip"))
-	if err != nil {
-		fmt.Println("FATAL: Unable to automount: missing discovery server IP Address")
-		Log.Fatal("Could not get ip", err)
-	}
-
-	port, err := ioutil.ReadFile(path.Join(pfsMeta, "port"))
-	if err != nil {
-		fmt.Println("FATAL: Unable to automount: missing discovery server port")
-		Log.Fatal("Could not get port", err)
-	}
-
 	mountpoint, err := ioutil.ReadFile(path.Join(pfsMeta, "mountpoint"))
 	if err != nil {
 		fmt.Println("FATAL: PFSD Couldnt find FS mountpoint", err)
 		Log.Fatal("Could not get mountpoint", err)
 	}
 
-	mountArgs := []string{string(ip) + ":" + string(port), args[0], string(mountpoint)}
+	mountArgs := []string{args[0], string(mountpoint)}
 	doMount(c, mountArgs)
 }
